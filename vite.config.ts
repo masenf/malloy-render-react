e9cdc337f4424023d9e0454e210d552d25575a32
skokenes <speroskokenes@gmail.com>
2024-05-11 18:33:12 +0300

    cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import dts from "vite-plugin-dts";
 export default defineConfig({
   plugins: [react(), dts()],
   build: {
+    minify: false,
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       formats: ["es"],
