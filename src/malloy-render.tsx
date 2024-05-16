fe20d6cf19974be7a7ae876bfc54b74b311152b7
skokenes <speroskokenes@gmail.com>
2024-05-16 16:35:26 +0300

    feat: direct ref

--- src/malloy-render.tsx
+++ src/malloy-render.tsx
@@ -1,48 +1,32 @@
 import "@malloydata/render";
-import { Result, QueryResult, ModelDef } from "@malloydata/malloy";
+import type { MalloyRenderProps } from "@malloydata/render";
 import React, { useEffect, useRef } from "react";
+
+type MalloyRenderElement = HTMLElement & MalloyRenderProps;
 
 declare global {
   namespace JSX {
     interface IntrinsicElements {
       "malloy-render": React.DetailedHTMLProps<
         React.HTMLAttributes<HTMLElement>,
-        HTMLElement
-      > & {
-        result?: Result;
-        queryResult?: QueryResult;
-        modelDef?: ModelDef;
-      }; // todo: malloy render props exports
+        MalloyRenderElement
+      >;
     }
   }
 }
-
-type MalloyRenderProps = {
-  result?: Result;
-  queryResult?: QueryResult;
-  modelDef?: ModelDef;
-};
 
 export function MalloyRender({
   result,
   queryResult,
   modelDef,
 }: MalloyRenderProps) {
-  const ref = useRef<HTMLDivElement>(null);
+  const ref = useRef<MalloyRenderElement>(null);
   useEffect(() => {
     if (ref.current) {
-      let render = ref.current.querySelector("malloy-render");
-      if (!render) {
-        render = document.createElement("malloy-render");
-        render.result = result;
-        render.queryResult = queryResult;
-        render.modelDef = modelDef;
-        ref.current.appendChild(render);
-      }
-      render.result = result;
-      render.queryResult = queryResult;
-      render.modelDef = modelDef;
+      ref.current.result = result;
+      ref.current.queryResult = queryResult;
+      ref.current.modelDef = modelDef;
     }
   }, [result, queryResult, modelDef]);
-  return <div ref={ref} />;
+  return <malloy-render ref={ref} />;
 }
